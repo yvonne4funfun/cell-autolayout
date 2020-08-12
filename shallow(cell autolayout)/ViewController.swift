2a817e8976103aba1dc5655c6a075dccfc1f9884
funfun <bubuorz@yahoo.com.tw>
2020-08-12 16:37:47 +0800

    Initial Commit

---
+++ shallow(cell autolayout)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shallow(cell autolayout)
+//
+//  Created by fun on 2020/8/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ shallow(cell autolayout)Tests/shallow_cell_autolayout_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  shallow_cell_autolayout_Tests.swift
+//  shallow(cell autolayout)Tests
+//
+//  Created by fun on 2020/8/12.
+//
+
+import XCTest
+@testable import shallow_cell_autolayout_
+
+class shallow_cell_autolayout_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
